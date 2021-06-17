28c41374f09d261c76f67c7de42ac23addda9531
trennich303 <trennich303@ed.amdsb.ca>
2021-06-17 11:51:04 -0700

    Add project files.

---
+++ ChooseYourOwnAdventure/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿
+namespace ChooseYourOwnAdventure
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.outputText = new System.Windows.Forms.Label();
+            this.buttonOption1 = new System.Windows.Forms.Button();
+            this.buttonOption2 = new System.Windows.Forms.Button();
+            this.labelOption1 = new System.Windows.Forms.Label();
+            this.labelOption2 = new System.Windows.Forms.Label();
+            this.labelOption3 = new System.Windows.Forms.Label();
+            this.buttonOption3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // outputText
+            // 
+            this.outputText.BackColor = System.Drawing.Color.Transparent;
+            this.outputText.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputText.ForeColor = System.Drawing.Color.Snow;
+            this.outputText.Location = new System.Drawing.Point(28, 9);
+            this.outputText.Name = "outputText";
+            this.outputText.Size = new System.Drawing.Size(760, 235);
+            this.outputText.TabIndex = 1;
+            this.outputText.Click += new System.EventHandler(this.outputText_Click);
+            // 
+            // buttonOption1
+            // 
+            this.buttonOption1.BackColor = System.Drawing.Color.DarkGray;
+            this.buttonOption1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonOption1.Location = new System.Drawing.Point(33, 290);
+            this.buttonOption1.Name = "buttonOption1";
+            this.buttonOption1.Size = new System.Drawing.Size(86, 58);
+            this.buttonOption1.TabIndex = 2;
+            this.buttonOption1.UseVisualStyleBackColor = false;
+            this.buttonOption1.Click += new System.EventHandler(this.buttonOption1_Click);
+            // 
+            // buttonOption2
+            // 
+            this.buttonOption2.BackColor = System.Drawing.Color.DarkGray;
+            this.buttonOption2.BackgroundImage = global::ChooseYourOwnAdventure.Properties.Resources._24_512;
+            this.buttonOption2.Location = new System.Drawing.Point(33, 369);
+            this.buttonOption2.Name = "buttonOption2";
+            this.buttonOption2.Size = new System.Drawing.Size(86, 54);
+            this.buttonOption2.TabIndex = 3;
+            this.buttonOption2.UseVisualStyleBackColor = false;
+            this.buttonOption2.Click += new System.EventHandler(this.buttonOption2_Click);
+            // 
+            // labelOption1
+            // 
+            this.labelOption1.AutoSize = true;
+            this.labelOption1.BackColor = System.Drawing.Color.Transparent;
+            this.labelOption1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelOption1.ForeColor = System.Drawing.Color.White;
+            this.labelOption1.Location = new System.Drawing.Point(147, 304);
+            this.labelOption1.Name = "labelOption1";
+            this.labelOption1.Size = new System.Drawing.Size(106, 29);
+            this.labelOption1.TabIndex = 4;
+            this.labelOption1.Text = "Go right";
+            // 
+            // labelOption2
+            // 
+            this.labelOption2.AutoSize = true;
+            this.labelOption2.BackColor = System.Drawing.Color.Transparent;
+            this.labelOption2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelOption2.ForeColor = System.Drawing.Color.White;
+            this.labelOption2.Location = new System.Drawing.Point(147, 381);
+            this.labelOption2.Name = "labelOption2";
+            this.labelOption2.Size = new System.Drawing.Size(90, 29);
+            this.labelOption2.TabIndex = 5;
+            this.labelOption2.Text = "Go left";
+            // 
+            // labelOption3
+            // 
+            this.labelOption3.BackColor = System.Drawing.Color.Transparent;
+            this.labelOption3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelOption3.ForeColor = System.Drawing.Color.White;
+            this.labelOption3.Location = new System.Drawing.Point(147, 235);
+            this.labelOption3.Name = "labelOption3";
+            this.labelOption3.Size = new System.Drawing.Size(106, 29);
+            this.labelOption3.TabIndex = 6;
+            // 
+            // buttonOption3
+            // 
+            this.buttonOption3.BackColor = System.Drawing.Color.DarkGray;
+            this.buttonOption3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonOption3.Location = new System.Drawing.Point(33, 221);
+            this.buttonOption3.Name = "buttonOption3";
+            this.buttonOption3.Size = new System.Drawing.Size(86, 58);
+            this.buttonOption3.TabIndex = 7;
+            this.buttonOption3.UseVisualStyleBackColor = false;
+            this.buttonOption3.Visible = false;
+            this.buttonOption3.Click += new System.EventHandler(this.buttonOption3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::ChooseYourOwnAdventure.Properties.Resources.amazon_rainforest;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonOption3);
+            this.Controls.Add(this.labelOption3);
+            this.Controls.Add(this.labelOption2);
+            this.Controls.Add(this.labelOption1);
+            this.Controls.Add(this.buttonOption2);
+            this.Controls.Add(this.buttonOption1);
+            this.Controls.Add(this.outputText);
+            this.Name = "Form1";
+            this.Text = " ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label outputText;
+        private System.Windows.Forms.Button buttonOption1;
+        private System.Windows.Forms.Button buttonOption2;
+        private System.Windows.Forms.Label labelOption1;
+        private System.Windows.Forms.Label labelOption2;
+        private System.Windows.Forms.Label labelOption3;
+        private System.Windows.Forms.Button buttonOption3;
+    }
+}
+
---
+++ ChooseYourOwnAdventure/Form1.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace ChooseYourOwnAdventure
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            DisplayPage();
+        }
+        double percentage;
+        int page = 1;
+        Random randGen = new Random();
+        
+        
+
+        private void outputText_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonOption1_Click(object sender, EventArgs e)
+        {
+            if (page == 1)
+            {
+                // int rand = randGen.Next(1,100)
+                // if rand is less than 70 go to page 6
+                // else go to page 10
+                page = 2;
+            }
+            else if (page == 2) { page = 5; }
+            else if (page == 3) { page = 1; }
+            else if (page == 4) { page = 1; }
+            else if (page == 5) { page = 6; }
+            else if (page == 6) { page = 1; }
+            else if (page == 7) { page = 8; }
+            else if (page == 8) { page = 9; }
+            else if (page == 9) { page = 1; }
+            else if (page == 10) { page = 11; }
+            else if (page == 11) { page = 1; }
+            else if (page == 12) { page = 1; }
+            else if (page == 13) { page = 1; }
+
+            DisplayPage();
+        }
+        private void buttonOption2_Click(object sender, EventArgs e)
+        {
+            if (page == 1)
+            {
+                // int rand = randGen.Next(1,100)
+                // if rand is less than 70 go to page 6
+                // else go to page 10
+                page = 4;
+            }
+            else if (page == 2) { page = 3; }
+            else if (page == 3) { page = 14; }
+            else if (page == 4) { page = 14; }
+            else if (page == 5) { page = 7; }
+            else if (page == 6) { page = 14; }
+            else if (page == 7) { page = 12; }
+            else if (page == 8) { page = 10; }
+            else if (page == 9) { page = 14; }
+            else if (page == 10) { page = 13; }
+            else if (page == 11) { page = 14; }
+            else if (page == 12) { page = 14; }
+            else if (page == 13) { page = 14; }
+
+            DisplayPage();
+        }
+
+
+        public void DisplayPage()
+        {
+            switch (page)
+            {
+                case 1:
+                    //  musicPlayer = new SoundPlayer(Properties.Resources.jungle);
+                    //  musicPlayer.Play();
+
+                    this.BackgroundImage = Properties.Resources.amazon_rainforest;
+
+                    outputText.Text = "You wakeup in the Amazon Rainforest from a plane crash, " +
+                        "you need to find supplies but they are all in the tail section of the crash " +
+                        "which is further in the forest. You stumble across two pathways, the pathway" +
+                        " infront of you will get you to the plane crash faster but it is in the more " +
+                        "dense and dangourus part of the forest. The pathway to the left is the longer yet safer route";
+                    labelOption1.Text = "Go left";
+                    labelOption2.Text = "Go straight";
+                    break;
+                case 2:
+                    //musicPlayer = new SoundPlayer(Properties.Resources.brook);
+                    //  musicPlayer.Play();
+
+                    this.BackgroundImage = Properties.Resources.secret_cave_260nw_175283300__1_;
+
+                    outputText.Text = "You come to a small clearing in the forest, but it starts to rain, do you take shelter in the cave to your left or do you continue thorugh the forest?";
+                    labelOption1.Text = "Continue on your path";
+                    labelOption2.Text = "Take shelter in the cave";
+                    break;
+               
+                case 3:
+                    
+                    outputText.Text = "You take shelter in the cave but you realize that it is a jaguar den and they eat you," +
+                        "do you want to play again?";
+                    labelOption1.Text = "Yes";
+                    labelOption2.Text = "No";
+                    break;
+                case 4:
+                    this.BackgroundImage = Properties.Resources.cottonmouth_snake_bushes_everglades_florida_600w_1026182830;
+                    outputText.Text = "While walking thorugh the dense forest you step on a snake and it bites you" +
+                       " and kills you with its venom, do you want to play again?";
+                    labelOption1.Text = "Yes";
+                    labelOption2.Text = "No";
+                    break;
+                case 5:
+                    this.BackgroundImage = Properties.Resources.Lake_in_the_Forest__9654486219_;
+                    buttonOption3.Visible = true;
+                    outputText.Text = "As you get closer and closer to the wreckage of the plane, you come across" +
+                        " a lake with two pathways on the left and right. The pathway in the left is half submerged in water.";
+                    labelOption1.Text = "Swim through the lake";
+                    labelOption2.Text = "Take the pathway on the right";
+                    labelOption3.Text = "Go left";
+                    break;
+                case 6:
+                    this.BackgroundImage = Properties.Resources.spectacled_caiman;
+                    buttonOption3.Visible = false;
+                    labelOption3.Text = "";
+                    outputText.Text = "The water has Caiman in it and they eat you, do you want to play again?";
+                    labelOption1.Text = "Yes";
+                    labelOption2.Text = "No";
+                    break;
+                case 7:
+                    this.BackgroundImage = Properties.Resources.Plane_Crash;
+                    buttonOption3.Visible = false;
+                    labelOption3.Text = "";
+                    outputText.Text = "After getting past the river, you come across the tail section of the plane," +
+                        " you find food, clean water, and flares. It has become night time, you can continue through the " +
+                        "forest with your extra supplies or wait it out in the shleter of the plane. ";
+                    labelOption1.Text = "Wait it out";
+                    labelOption2.Text = "Continue through the forest";
+                    break;
+                case 8:
+                    outputText.Text =  "You wait it out till morning and gain some strength from sleeping and eating some" +
+                        " of the supplies, you can light the flare where the plane crashed, but in the thick forest it" +
+                        "is harder for someone to find you, or you can continue on the original path to the " +
+                        "less dense part of the forest ";
+                    labelOption1.Text = "Light flare here";
+                    labelOption2.Text = "Continue on your original path";
+
+                    break;
+                case 9:
+                    outputText.Text = "No one sees the flare and you die of starvation," +
+                        "do you want to play again?"; 
+                    labelOption1.Text = "Yes";
+                    labelOption2.Text = "No";
+                    break;
+                case 10:
+                    this.BackgroundImage = Properties.Resources.waterfall;
+                    outputText.Text = "You are walking along your path near a waterfall when a " +
+                        "A jaguar nearby spots you, do you try to scale a tree to escape or jump into the ravine?";
+                    labelOption1.Text = "Climb the tree";
+                    labelOption2.Text = "Jump into the ravine";
+                    
+                    break;
+                case 11:
+                    this.BackgroundImage = Properties.Resources.tree2;
+                    outputText.Text = "You figure out the hard way that jaguars can climb trees, " +
+                        "do you want to play again?";
+                    labelOption1.Text = "Yes";
+                    labelOption2.Text = "No";
+                    break;
+                case 12:
+                    this.BackgroundImage = Properties.Resources.Night_Time;
+                    outputText.Text = "While walking in the dark you trip and fall off a clif, do you want to play again?";
+                    labelOption1.Text = "Yes";
+                    labelOption2.Text = "No";
+                    break;
+                case 13:
+                    this.BackgroundImage = Properties.Resources.Rescue_Helicopter;
+                    outputText.Text = "You escaped the jaguar, after lighting the flare in the open clearing a" +
+                        " rescue helicopter finds you after a couple hours. Do you want to play again?";
+                    labelOption1.Text = "Yes";
+                    labelOption2.Text = "No";
+                    break;
+                case 14:
+                    outputText.Text = "Thank you for playing, do you want to play again";
+                    labelOption1.Text = "Yes";
+                    labelOption2.Text = "No";
+
+                    Refresh();
+                    Thread.Sleep(2000);
+                    Application.Exit();
+                    break;
+                default:
+                    break;
+            }
+        }
+        
+        private void buttonOption3_Click(object sender, EventArgs e)
+        {
+            percentage = randGen.Next(1, 101);
+            if (page == 5 && percentage >= 70)
+            {
+                page = 7;
+            }
+            else if (page ==5)
+            {
+                page = 6;
+            }
+            DisplayPage();
+        }
+    }
+}
